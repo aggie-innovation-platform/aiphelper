e1efb667dedaca5cbc53ab71d2ab521f7bfe5cc0
joraff <joraff@gmail.com>
2022-04-26 10:35:24 -0500

    Reverts Regions to type not function

--- aws/command.go
+++ aws/command.go
@@ -20,11 +20,10 @@ var (
 )
 
 type Options struct {
-	SSOStartURL string   `long:"sso-start-url" default:"https://aggie-innovation-platform.awsapps.com/start" description:"AWS SSO Start URL"`
-	SSORegion   string   `long:"sso-region" default:"us-east-2" description:"AWS SSO Region"`
-	SSORoleName string   `long:"sso-role-name" default:"AdministratorAccess" description:"SSO Role To Assume (must be the same across all accounts)"`
-	Regions     *Regions `long:"regions" default:"" description:"Comma-separated list of regions to tell Steampipe to connect to (default: uses same search order as aws cli)"`
-	// Regions       func(r string) []string `long:"regions" description:"Comma-separated list of regions to tell Steampipe to connect to (default: uses same search order as aws cli)"`
+	SSOStartURL   string    `long:"sso-start-url" default:"https://aggie-innovation-platform.awsapps.com/start" description:"AWS SSO Start URL"`
+	SSORegion     string    `long:"sso-region" default:"us-east-2" description:"AWS SSO Region"`
+	SSORoleName   string    `long:"sso-role-name" default:"AdministratorAccess" description:"SSO Role To Assume (must be the same across all accounts)"`
+	Regions       *Regions  `long:"regions" default:"" description:"Comma-separated list of regions to tell Steampipe to connect to (default: uses same search order as aws cli)"`
 	Accounts      *Accounts `long:"accounts" default:"" description:"Comma-separated list of accounts to tell Steampipe to connect to (default: all accounts assigned to you through SSO)"`
 	DefaultFormat string    `long:"output-format" default:"json" description:"Output format for AWS CLI"`
 	DefaultRegion string    `long:"default-region" default:"us-east-1" description:"Default region for AWS CLI operations"`
@@ -32,14 +31,6 @@ type Options struct {
 
 func AddCommand(p *flags.Parser) {
 	options = &Options{}
-	// options.Regions = func(r string) []string {
-	// 	var tempValue = utils.SplitArgumentParser(r)
-	// 	if len(tempValue) == 0 {
-	// 		return []string{}
-	// 	}
-	// 	return tempValue
-	// }
-
 	p.AddCommand("aws", "Initialize AWS", "Initialize AWS", options)
 }
 
